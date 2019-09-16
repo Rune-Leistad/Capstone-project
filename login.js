722dd570580a0aaa6c7c71497fc71dbac2b50251
Taylor Green <taylordanielgreen@gmail.com>
2019-09-16 22:39:40 +1000

    Many major updates
    Login, registration, backend and games all now working

--- login.js
+++ login.js
@@ -27,6 +27,47 @@ app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/login.html'));
 });
 
+app.get('/style.css', function(request, response) {
+	response.sendFile(__dirname + "/style.css");
+});
+
+app.get('/home.html', function(request, response) {
+	response.sendFile(__dirname + "/home.html");
+	if (request.session.loggedin) {
+		console.log("Logged in!");
+	}
+});
+
+app.get('/chat.html', function(request, response) {
+	response.sendFile(__dirname + "/chat.html");
+});
+
+app.get('/games.html', function(request, response) {
+	response.sendFile(__dirname + "/games.html");
+});
+
+app.get('/learning-materials.html', function(request, response) {
+	response.sendFile(__dirname + "/learning-materials.html");
+});
+
+app.get('/register.html', function(request, response) {
+	response.sendFile(__dirname + "/register.html");
+});
+
+app.get('/statistics.html', function(request, response) {
+	response.sendFile(__dirname + "/statistics.html");
+});
+
+app.get('/tutor-request.html', function(request, response) {
+	response.sendFile(__dirname + "/tutor-request.html");
+});
+
+app.get('/video.html', function(request, response) {
+	response.sendFile(__dirname + "/video.html");
+});
+
+
+
 app.post('/auth', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
@@ -36,7 +77,7 @@ app.post('/auth', function(request, response) {
 				console.log("MATCH");
 				request.session.loggedin = true;
 				request.session.username = username;
-				response.redirect('/home');
+				response.redirect("/home.html");
 			} else {
 				response.send('Incorrect Username and/or Password!');
 			}			
@@ -48,14 +89,29 @@ app.post('/auth', function(request, response) {
 	}
 });
 
+
+
+app.post('/reg', function(request, response) {
+	con.query('INSERT INTO `user_information`.`log_in_data` (`user_name`, `user_email`, `user_password`, `user_secQ`, `user_secQAns`, `user_state`, `user_gender`, `user_phone`) VALUES (?, ?, ?, ?, ?, ?, "Male", ?);', [request.body.firstname, request.body.email, request.body.password, request.body.question,request.body.answer, request.body.state, request.body.phone], function(error, results, fields){});
+	request.session.loggedin = true;
+	request.session.username = request.body.email;
+	response.redirect("/home.html");
+});
+
+
+
+
+
+
 app.get('/home', function(request, response) {
 	if (request.session.loggedin) {
-		console.log("Logged in!");
-		response.sendFile(path.join(__dirname + '/home.html'));
+		request.method = 'get';
+		response.sendFile(__dirname + "/home.html");
 	} else {
 		response.send('Please login to view this page!');
 	}
 	response.end();
 });
 app.use('/', router);
+app.use('/img', express.static('img'));
 app.listen(process.env.port || 3000);
