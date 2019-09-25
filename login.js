c08f7b23f123ad1c0737ba65ff409ad983dbdea0
exlipy <41883655+exlipy@users.noreply.github.com>
2019-09-25 18:48:19 +1000

    Move to ejs

--- login.js
+++ login.js
@@ -5,9 +5,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
 var router = express.Router();
-
-var sql = "SELECT * FROM user_information.log_in_data WHERE user_id = 1;"
-var sql1 = "SELECT * FROM user_information.forum_posts WHERE user_id = 1;"
+var expressLayouts = require('express-ejs-layouts');
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -22,56 +20,65 @@ app.use(session({
 }));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+app.use(expressLayouts);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+    response.render('home', {page_name: 'home'});
 });
+
 
 app.get('/style.css', function(request, response) {
 	response.sendFile(__dirname + "/style.css");
 });
 
-app.get('/home.html', function(request, response) {
-	response.sendFile(__dirname + "/home.html");
-	if (request.session.loggedin) {
-		console.log("Logged in!");
-	}
+
+app.get('/home', function(request, response) {
+    con.query('SELECT * FROM user_information.forum_posts', function(error, results, fields) {
+      response.render('home', {
+                      page_name: 'home',
+                      logged_in: request.session.loggedin,
+                      posts: results});
+    });
+
 });
 
-app.get('/chat.html', function(request, response) {
-	response.sendFile(__dirname + "/chat.html");
+app.get('/chat', function(request, response) {
+	response.render('chat', {page_name: 'chat', logged_in: request.session.loggedin});
 });
 
-app.get('/games.html', function(request, response) {
-	response.sendFile(__dirname + "/games.html");
+app.get('/games', function(request, response) {
+	response.render('games', {page_name: 'games', logged_in: request.session.loggedin});
 });
 
-app.get('/learning-materials.html', function(request, response) {
-	response.sendFile(__dirname + "/learning-materials.html");
+app.get('/learning-materials', function(request, response) {
+	response.render('learning-materials', {page_name: 'learning-materials', logged_in: request.session.loggedin});
 });
 
-app.get('/register.html', function(request, response) {
-	response.sendFile(__dirname + "/register.html");
+app.get('/register', function(request, response) {
+	response.render('register', {page_name: 'register', logged_in: request.session.loggedin});
 });
 
-app.get('/statistics.html', function(request, response) {
-	response.sendFile(__dirname + "/statistics.html");
+app.get('/statistics', function(request, response) {
+	response.render('statistics', {page_name: 'statistics', logged_in: request.session.loggedin});
 });
 
-app.get('/tutor-request.html', function(request, response) {
-	response.sendFile(__dirname + "/tutor-request.html");
+app.get('/tutor-request', function(request, response) {
+	response.render('tutor-request', {page_name: 'tutor-request', logged_in: request.session.loggedin});
 });
 
-app.get('/video.html', function(request, response) {
-	response.sendFile(__dirname + "/video.html");
+app.get('/video', function(request, response) {
+	response.render('video', {page_name: 'video', logged_in: request.session.loggedin});
 });
 
-app.get('/changepass.html', function(request, response) {
-	response.sendFile(__dirname + "/changepass.html");
+app.get('/changepass', function(request, response) {
+	response.render('changepass', {page_name: 'changepass', logged_in: request.session.loggedin});
 });
 
-app.get('/login.html', function(request, response) {
-	response.sendFile(__dirname + "/login.html");
+app.get('/login', function(request, response) {
+	response.render('login', {page_name: 'login', logged_in: request.session.loggedin});
 });
 
 app.post('/auth', function(request, response) {
@@ -83,11 +90,12 @@ app.post('/auth', function(request, response) {
     });
 		con.query('SELECT * FROM user_information.log_in_data WHERE user_email = ? AND user_password = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
-                
+
 				console.log("MATCH");
 				request.session.loggedin = true;
 				request.session.username = username;
-				response.redirect("/home.html");
+                request.session.userid = results[0].user_id; // results[0] should always be the logged in user
+				response.redirect("/home");
 			} else {
 				response.send('Incorrect Username and/or Password!');
 			}
@@ -105,7 +113,7 @@ app.post('/reg', function(request, response) {
 	con.query('INSERT INTO `user_information`.`log_in_data` (`user_name`, `user_email`, `user_password`, `user_secQ`, `user_secQAns`, `user_state`, `user_gender`, `user_phone`) VALUES (?, ?, ?, ?, ?, ?, "Male", ?);', [request.body.firstname, request.body.email, request.body.password, request.body.question,request.body.answer, request.body.state, request.body.phone], function(error, results, fields){});
 	request.session.loggedin = true;
 	request.session.username = request.body.email;
-	response.redirect("/home.html");
+	response.redirect("/home");
 });
 
 // Update password
@@ -113,24 +121,27 @@ app.post('/chng', function(request, response) {
 	con.query('UPDATE `user_information`.`log_in_data` SET `user_password` = ? WHERE (`user_email` = ? AND `user_secQAns` = ?);', [request.body.password, request.body.username, request.body.answer], function(error, results, fields){});
 	request.session.loggedin = true;
 	request.session.username = request.body.email;
-	response.redirect("/home.html");
+	response.redirect("/home");
 });
 
 // Creating noticeboard posts
 app.post('/newpost', function(request, response) {
-    con.query('INSERT INTO `user_information`.`forum_posts` (`user_id`, `post_language`, `post_topic`, `post_content`) VALUES ("1", "ENG", ?, ?);', [request.body.topic, request.body.content], function(error, results, fields){console.log(error)});
-    response.redirect("/home.html");
+    con.query('INSERT INTO `user_information`.`forum_posts` (`user_id`, `post_language`, `post_topic`, `post_content`, `post_views`) VALUES (?, "ENG", ?, ?, "0");', [request.session.userid, request.body.topic, request.body.content], function(error, results, fields){console.log(error)});
+    console.log("Noticeboard post successfully saved");
+    response.redirect("/home");
 });
 
 app.get('/home', function(request, response) {
 	if (request.session.loggedin) {
 		request.method = 'get';
-		response.sendFile(__dirname + "/home.html");
+		response.render('home');
 	} else {
 		response.send('Please login to view this page!');
 	}
 	response.end();
 });
+
+
 app.use('/', router);
 app.use('/img', express.static('img'));
 app.listen(process.env.port || 3000);
